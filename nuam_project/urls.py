2c683e7f6de08e3ca7405199e5f9590b000575bf
caracolinn <gonzalo.mercado04@inacapmail.cl>
2025-11-05 02:00:50 -0400

    completado para revision

--- nuam_project/urls.py
+++ nuam_project/urls.py
@@ -4,13 +4,15 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # --- ¡CAMBIO! ---
+    # Ahora la raíz del sitio ('') es la página de Login
+    path('', auth_views.LoginView.as_view(template_name='calificaciones/login.html'), name='login'),
+    
     path('admin/', admin.site.urls),
     
-    path('login/', auth_views.LoginView.as_view(template_name='calificaciones/login.html'), name='login'),
-    
-    # --- ¡LÍNEA MODIFICADA! ---
-    # Le decimos que también acepte 'get' para cerrar sesión
+    # La ruta de logout se mantiene
     path('logout/', auth_views.LogoutView.as_view(http_method_names=['post', 'get', 'options']), name='logout'),
 
+    # La ruta de tu aplicación
     path('calificaciones/', include('calificaciones.urls')),
 ]
